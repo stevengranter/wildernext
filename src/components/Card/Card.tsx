86ce7022e1d3eb2e22ef19d0156d518f6b0f6695
Steven Granter <stevengranter@gmail.com>
2025-02-17 18:01:56 -0330

    Revised Card component (WIP)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,7 @@
 import { PropsWithChildren } from 'react'
+import clsx from 'clsx'
+import Image from 'next/image'
+import imagePlaceholder from 'images/canada-geese.jpg'
 
 type CardProps = {
     flipped?: boolean
@@ -9,12 +12,27 @@ type CardProps = {
 type Props = PropsWithChildren<CardProps>
 
 export function Card({ flipped = false, title = 'Title', children }: Props) {
+    const imageClass = clsx('rounded-t-lg aspect-square object-cover', {
+        'blur-sm': flipped,
+    })
     return (
         <div>
-            <div className="rounded-md shadow-md p-3 m-3 bg-amber-50">
-                <div>{!flipped ? 'Front' : 'Back'}</div>
-                {title && <span className="font-bold ">{title}</span>}
-                {children}
+            <div className="max-w-sm rounded-lg shadow-sm dark:bg-gray-800">
+                <a href="#">
+                    <Image
+                        className={imageClass}
+                        src={imagePlaceholder}
+                        alt=""
+                    />
+                </a>
+                <div className="p-5">
+                    <a href="#">
+                        <h5 className="mb-2 text-xl font-bold text-gray-900 dark:text-white">
+                            {title}
+                        </h5>
+                    </a>
+                    {children}
+                </div>
             </div>
         </div>
     )
